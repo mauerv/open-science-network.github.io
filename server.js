7a8a0cdc5f8fd2069648ededbb0a3bb6ecf6c448
Mauro Velazquez <maurovelazquez1@gmail.com>
2017-12-05 11:42:50 -0300

    Improve CORS security

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser')
 app.use(express.static(__dirname))
 app.use(bodyParser.urlencoded())
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://www.osn.global");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
