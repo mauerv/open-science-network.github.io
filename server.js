487fc2e8930de16a1d6c127833316daeb555e598
Mauro Velazquez <maurovelazquez1@gmail.com>
2018-05-08 16:46:34 -0300

    Add token landing

--- server.js
+++ server.js
@@ -1,21 +1,26 @@
 const express = require('express')
 const app = express()
-const fetch = require('node-fetch')
+const subdomain = require('express-subdomain')
 const bodyParser = require('body-parser')
 const sslRedirect = require('heroku-ssl-redirect')
+
+const router = express.Router()
+
+router.get('/', function(req, res) {
+  let path = __dirname + '/token.html'
+  res.sendFile(path)
+})
+
+app.use(subdomain('token', router))
 
 app.use(sslRedirect())
 app.use(express.static(__dirname))
 app.use(bodyParser.urlencoded())
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://www.osn.global");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 app.get('/', function(req, res) {
   let path = __dirname + '/index.html'
   res.sendFile(path)
 });
+
 
 app.listen(process.env.PORT || 8080)
